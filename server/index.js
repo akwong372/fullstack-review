dcfcab1e655246a6291db35cc5a2faf61cb125ab
akwong372 <akwong372@gmail.com>
2019-01-22 20:43:54 -0800

    Complete Bare Minimum Requirements

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
@@ -20,7 +19,6 @@ app.post('/repos', function (req, res) {
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should send back the top 25 repos
 
   database.findTop()
